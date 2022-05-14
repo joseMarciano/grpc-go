243de014511800cb894f4a9ed2885660d2d76d64
joseMarciano <marcianojosepaulo@gmail.com>
2022-05-14 09:42:27 -0300

    feat: implementing client stream in server

--- services/user.go
+++ services/user.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/joseMarciano/grpc-go/pb/pb"
+	"io"
+	"log"
 	"time"
 )
 
@@ -88,6 +90,37 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	}
 
 	time.Sleep(time.Second * 3)
+
+	return nil
+}
+
+//AddUsers(UserService_AddUsersServer) error
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding ", req.GetName())
+
+	}
 
 	return nil
 }
